b57a4e22925f2bb1c83165836c62c84a1d3d0213
Yoshihiro Seki <nikochan2k@gmail.com>
2022-03-28 21:40:24 +0900

    modify testcase

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,18 +1,17 @@
-import { S3 } from "aws-sdk";
+import { NotFoundError } from "univ-fs";
 import { S3FileSystem } from "../S3FileSystem";
 import config from "./secret.json";
 
 export const fs = new S3FileSystem("univ-fs-test", "test", config);
 
 export const setup = async () => {
-  const client = new S3(config);
-  const data = await client.listObjectsV2({ Bucket: "univ-fs-test" }).promise();
-  for (const content of data.Contents || []) {
-    await client
-      .deleteObject({
-        Bucket: "univ-fs-test",
-        Key: content.Key as string,
-      })
-      .promise();
+  try {
+    const root = await fs._getDirectory("/");
+    await root.rm({ force: true, recursive: true, ignoreHook: true });
+    await root.mkdir({ force: true, recursive: false, ignoreHook: true });
+  } catch (e) {
+    if (e.name !== NotFoundError.name) {
+      throw e;
+    }
   }
 };
